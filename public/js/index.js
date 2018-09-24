acd4fe0135ac0ac6e21e06a7841ce9dcb1e2b505
luca <grillo.luca@gmail.com>
2018-09-24 18:47:32 +0200

    addedd bulma.io

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,4 @@
+
 var socket = io();
 socket.on('connect', function () {
     console.log("Connected to the server");
@@ -11,7 +12,7 @@ socket.on('newMessage', function (message) {
     console.log("new Message", message);
 
     var node = document.createElement("li"); 
-    var textnode = document.createTextNode(`${message.createdAt} [${message.from}]: ${message.text}`);
+    var textnode = document.createTextNode(`[${message.from}]: ${message.text}`);
     node.appendChild(textnode);
     document.getElementById("messages").appendChild(node);
     
@@ -30,7 +31,6 @@ onSendMessage = () => {
         text: document.getElementById('txtInput').value, 
         createdAt: new Date().getTime()
     }, function(data) {
-        console.log('Got it!', data);
+        txtInput.value = "";
     });
-    txtInput.value = "";
 }
